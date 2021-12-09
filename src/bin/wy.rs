6e46943663be31f6120bc6a3a7a51c634936509f
DavePearce <david.pearce@ecs.vuw.ac.nz>
2021-12-10 09:35:08 +1300

    Add cli parsing
    This adds support for parsing command-line arguments via clap.  I've only
    sketched it out, but seems to do what I want.

--- src/bin/wy.rs
+++ src/bin/wy.rs
@@ -1,3 +1,24 @@
+use clap::{App, AppSettings};
+
 pub fn main() {
-    println!("Hello Whiley World");
+    // Parse command-line arguments
+    let matches = App::new("wy")
+	.about("Whiley's Build Tool and Package Manager")
+	.version("0.6.0")
+	.setting(AppSettings::SubcommandRequiredElseHelp)
+	.subcommand(
+	    App::new("build").about("Build local package(s)"))
+	.subcommand(
+	    App::new("clean").about("Remove all generated build artifact(s)"))	
+	.get_matches();
+    // Dispatch on outcome
+    match matches.subcommand() {
+	("build", Some(_)) => {
+	    println!("Build not implemented yet!");
+	}
+	("clean", Some(_)) => {
+	    println!("Clean not implemented yet!");
+	}
+	_ => unreachable!()
+    }
 }
