b07fbcf7e75e088a3a4a6f48f6a2502ea40c7d64
DavePearce <david.pearce@ecs.vuw.ac.nz>
2022-01-11 17:28:41 +1300

    Minor tweaks
    This applies some minor tweaks to the Java Driver.  in essence, it works ... but
    for reasons unknown installing the command does not really work.  Anyway, more
    to think about.

--- src/bin/wy.rs
+++ src/bin/wy.rs
@@ -7,12 +7,11 @@ fn main() -> Result<(),J4RsError> {
     let jvm = JvmBuilder::new().build()?;
     //
     let empty_array = jvm.create_java_array("java.lang.String", &Vec::new())?;
-    //let args = vec![InvocationArg::try_from(empty_array.as_slice())?];
-    //
     let http_components = MavenArtifact::from("org.apache.httpcomponents:httpclient:4.5.13");
     let jbfs_artifact =  MavenArtifact::from("org.whiley:jbuildfs:1.0.1");
     let wycc_artifact = MavenArtifact::from("org.whiley:wycc:0.9.9");
     let wycli_artifact = MavenArtifact::from("org.whiley:wycli:0.9.9");
+    jvm.deploy_artifact(&http_components)?;        
     jvm.deploy_artifact(&jbfs_artifact)?;    
     jvm.deploy_artifact(&wycc_artifact)?;
     jvm.deploy_artifact(&wycli_artifact)?;    
