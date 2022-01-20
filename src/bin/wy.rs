fcd4a2f8eb89380c4dcffca5135272cfef65b058
DavePearce <david.pearce@ecs.vuw.ac.nz>
2022-01-20 16:51:34 +1300

    Fix #26
    This adds preliminary support for the "init" command which initialises a new
    project.  Eventually, more options will be required (e.g. to specify a new
    JavaScript project, etc).

--- src/bin/wy.rs
+++ src/bin/wy.rs
@@ -1,11 +1,8 @@
 use clap::{arg, App, AppSettings};
 use std::error::Error;
-use std::path::Path;
-use std::fs;
 use log::LevelFilter;
-use whiley::config::Config;
-use whiley::command::Build;
-use whiley::{init_logging,init_whileyhome,init_registry};
+use whiley::command::{build,init};
+use whiley::{init_logging,init_whileyhome};
 
 fn main() -> Result<(),Box<dyn Error>> {
     // Parse command-line arguments
@@ -16,6 +13,8 @@ fn main() -> Result<(),Box<dyn Error>> {
 	.arg(arg!(--verbose "Show verbose output"))
 	.subcommand(
 	    App::new("build").about("Build local package(s)"))
+	.subcommand(
+	    App::new("init").about("Create a new Whiley package in an existing directory"))	
 	.get_matches();
     // Extract top-level flags
     let verbose = matches.is_present("verbose");    
@@ -25,24 +24,10 @@ fn main() -> Result<(),Box<dyn Error>> {
     }
     // Initialise Whiley home directory
     let whileyhome = init_whileyhome();
-    // Read build configuration
-    let config_file = fs::read_to_string("wy.toml").expect("Error reading build configuration!");
-    // Parse configuration
-    let config = Config::from_str(config_file.as_str())?;
     // Dispatch on outcome
     match matches.subcommand() {
-	Some(("build", _)) => build(&config,&whileyhome),
+	Some(("build", _)) => build(&whileyhome),
+	Some(("init", _)) => init(&whileyhome),
 	_ => unreachable!()
     }
-}
-
-fn build(config: &Config, whileyhome: &Path) -> Result<(),Box<dyn Error>> {
-   // Initialise platform registry
-    let registry = init_registry();    
-    // Construct build plan
-    let build = Build::from_str(&config,&registry)?;
-    // Go!
-    build.run(whileyhome);
-    //
-    Ok(())
 }
